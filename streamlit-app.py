dbbf457b0fcf2cf3a75ae8b8721e300e8dfff2bd
Jonah Kanner <jkanner@caltech.edu>
2024-05-21 15:00:19 -0700

    adding vision statement

--- streamlit-app.py
+++ streamlit-app.py
@@ -12,17 +12,15 @@ st.write("## CalTrans D7 Active Transportation Dashboard")
 # -- Input Data
 
 year = np.arange(2023, 2040)
-st.write(year)
-
 carmiles = 1173*np.ones(len(year))
-st.write(carmiles)
-
 bikemiles = 0.01*carmiles
-st.write(bikemiles)
-
 bikegoal = 30*(year-year[0]) + bikemiles[0]
-st.write(bikegoal)
 
+# -- Header
+st.image('img/vision-caltrans.png')
+
+
+# -- Format data frame
 data = {
     'Year': year,
     'Highway Miles': carmiles,
@@ -36,3 +34,5 @@ df = pd.DataFrame(data=data)
 fig = px.line(df, x='Year', y=['Bike Lane Miles', 'Target'])
 
 st.plotly_chart(fig)
+
+
